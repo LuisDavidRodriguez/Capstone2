c50147f515d9796a74eb81362837dd94f322f6e5
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-29 19:16:53 -0300

    Add basic API functions
    This commit adds the functions of posting likes
    and getting likes from the Involvement API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles/main.scss';
 import { getShows } from './modules/tvmaze';
+import * as invApi from './modules/involvementApi';
 
-getShows();
+invApi.getLikes();
---
+++ src/modules/involvementApi.js
@@ -0,0 +1,24 @@
+const BASE_URL =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+
+const GAME_ID = '3bifdQ3qgzMtAvx1V3Pc';
+
+const getLikes = async () => {
+  const result = await fetch(`${BASE_URL}${GAME_ID}/likes`);
+  const data = await result.json();
+  console.log(data);
+};
+
+const addLike = async (name) => {
+  const body = JSON.stringify({
+    item_id: 'item1',
+  });
+  const headers = { 'Content-type': 'application/json; charset=UTF-8' }; 
+  const result = await fetch(`${BASE_URL}${GAME_ID}/likes`, {
+    method: 'POST',
+    body,
+    headers,
+  });
+};
+
+export { getLikes, addLike };
